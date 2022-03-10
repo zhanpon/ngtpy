fa93fc39c69995e0b99f80388addd00341cd15d0
Yiyang Zhan <pon.zhan@gmail.com>
2022-03-11 06:49:15 +0900

    Add a test for batch_insert

--- tests/test_ngtpy.py
+++ tests/test_ngtpy.py
@@ -18,3 +18,17 @@ def test_insert(tmp_path):
     result = index.get_object(v_id)
 
     assert result == v
+
+
+def test_batch_insert(tmp_path):
+    index = new_index(tmp_path, dimension=2)
+
+    v0 = [0, -1]
+    v1 = [0, 0]
+    v2 = [0, 1]
+    index.batch_insert([v0, v1, v2])
+
+    result = index.linear_search([0, 0.1], size=3)
+    result_ids = [i for i, _ in result]
+
+    assert result_ids == [1, 2, 0]
